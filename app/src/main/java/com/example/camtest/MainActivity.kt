ae44a2bf6e17d82fe7268d5773f6df290ea9c27f
David Mejicano <demoortiz1@gmail.com>
2025-02-07 15:21:55 -0600

    otra prueba de webhook

--- app/src/main/java/com/example/camtest/MainActivity.kt
+++ app/src/main/java/com/example/camtest/MainActivity.kt
@@ -25,7 +25,7 @@ import java.io.File
 class MainActivity : AppCompatActivity() {
 
 
-
+//coment
     @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
